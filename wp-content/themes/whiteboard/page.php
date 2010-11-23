24c8390633fff5dd6b2016b0c195d355cc18028f
Brian Andrich <brianandrich@Brian-Andrichs-MacBook.local>
2010-11-22 20:58:35 -0600

    lubbock

--- wp-content/themes/whiteboard/page.php
+++ wp-content/themes/whiteboard/page.php
@@ -7,7 +7,7 @@
 
 	<div id="map">
 	
-	<iframe width="848" height="398" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=http:%2F%2Fmaps.google.com%2Fmaps%2Fms%3Fdoflg%3Dptm%26ie%3DUTF8%26msa%3D0%26msid%3D104024361749664089066.000491c24f8b67becd2bc%26ll%3D36.04705,-105.82093%26spn%3D1.172525,2.469177%26z%3D9%26output%3Dkml&amp;sll=36.04705,-105.82093&amp;sspn=1.172525,2.469177&amp;ie=UTF8&amp;ll=39.061849,-109.204102&amp;spn=13.569582,37.22168&amp;z=5&amp;output=embed"></iframe>
+	<iframe width="848" height="398" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=http:%2F%2Fmaps.google.com%2Fmaps%2Fms%3Fie%3DUTF8%26hl%3Den%26oe%3DUTF8%26start%3D0%26num%3D200%26msa%3D0%26msid%3D104024361749664089066.000491c24f8b67becd2bc%26z%3D4%26output%3Dkml&amp;sll=47.608005,-134.07601&amp;sspn=31.327672,79.013672&amp;ie=UTF8&amp;ll=36.491973,-108.544922&amp;spn=14.045006,37.22168&amp;z=5&amp;output=embed"></iframe>
 		
 		
 		</div>
