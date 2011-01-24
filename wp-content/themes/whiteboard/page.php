f9dd1f291c4be5c9da5db3714acc29b4a13571e0
Brian Andrich <brianandrich@Brian-Andrichs-MacBook.local>
2011-01-24 12:39:42 -0500

    ky

--- wp-content/themes/whiteboard/page.php
+++ wp-content/themes/whiteboard/page.php
@@ -7,7 +7,7 @@
 
 	<div id="map">
 	
-<iframe width="848" height="398" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=http:%2F%2Fmaps.google.com%2Fmaps%2Fms%3Fsource%3Ds_q%26hl%3Den%26geocode%3D%26ie%3DUTF8%26oe%3DUTF8%26start%3D0%26num%3D200%26msa%3D0%26msid%3D207264463711218357759.000491c24f8b67becd2bc%26z%3D4%26output%3Dkml&amp;aq=&amp;sll=47.608005,-134.07601&amp;sspn=31.327672,79.013672&amp;ie=UTF8&amp;ll=34.198173,-91.889648&amp;spn=14.445373,37.22168&amp;z=5&amp;output=embed"></iframe>
+<iframe width="848" height="398" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=http:%2F%2Fmaps.google.com%2Fmaps%2Fms%3Fdoflg%3Dptm%26ie%3DUTF8%26hl%3Den%26oe%3DUTF8%26start%3D0%26num%3D200%26msa%3D0%26msid%3D207264463711218357759.000491c24f8b67becd2bc%26z%3D4%26output%3Dkml&amp;aq=&amp;sll=47.81396,-134.07601&amp;sspn=31.226383,79.013672&amp;ie=UTF8&amp;ll=34.560859,-90.527344&amp;spn=14.383601,37.22168&amp;z=5&amp;output=embed"></iframe>
 		
 		</div>
 		
