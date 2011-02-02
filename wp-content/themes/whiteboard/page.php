4fedb1ed92788407f9b38658df99163b79f5f74b
Brian Andrich <brianandrich@Brian-Andrichs-MacBook.local>
2011-02-02 15:33:03 -0500

    maps26

--- wp-content/themes/whiteboard/page.php
+++ wp-content/themes/whiteboard/page.php
@@ -10,7 +10,6 @@
 function initialize() {
      if (GBrowserIsCompatible()) {
 	        geoXml = new GGeoXml("http://beastranger.net/b.kml");
-			geoXml = new GGeoXml("http://beastranger.net/a.kml");
 	        map = new GMap2(document.getElementById("map_canvas")); 
 	        map.setCenter(new GLatLng(48.458352,-94.921875), 3); 
 	        map.setUIToDefault();
